34abdd680f9c76c786bf65d8a39f6d202c6385b0
Habibaeo123 <63856368+Habibaeo123@users.noreply.github.com>
2020-06-07 14:07:21 -0700

    Heading inserted

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Class 2 Assignment</h1>
         <p>
          Hello World from Habib Ur Rehman
         </p>
